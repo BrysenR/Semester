e2d514a7ae0d9a9d3c834fc74421dab2a6856f92
Br3300 <45640200+Br3300@users.noreply.github.com>
2019-01-02 15:07:33 -0600

    Update Semester.java

--- Semester.java
+++ Semester.java
@@ -16,39 +16,39 @@ public class Semester
     	System.out.println("Enter your characters name: (Male name reccomended)");
     	String name = scan.nextLine();
    		System.out.println("Welcome to the story! Now, for the prolog:\n\n\n");
-   		System.out.println("Itís a story youíve heard a thousand times. \"The princess is trapped in the castle, guarded by a ");
+   		System.out.println("It‚Äôs a story you‚Äôve heard a thousand times. \"The princess is trapped in the castle, guarded by a ");
    		System.out.println("dragon, asleep in a magic spell.\" A child of poverty, you listen to your grandmother tell you the ");
    		System.out.println("story again. You dream of going to the castle, slaying the beast and taking home the woman of ");
    		System.out.println("your dreams. With a sigh, you finish your bowl of bland porridge and start to rise. \n\n");
-   		System.out.println("\"Hasnít anyone told you to listen when your elders are speaking?\" my grandmother's voice ");
+   		System.out.println("\"Hasn‚Äôt anyone told you to listen when your elders are speaking?\" my grandmother's voice ");
    		System.out.println("brings me back to reality. \"Sorry grandmother\" you apologize, \"I was distracted.\" \"Distracted?\" ");
-   		System.out.println("\"Youíre always distracted lately. Youíre a man now, itís time to start acting like one...\" she starts ");
+   		System.out.println("\"You‚Äôre always distracted lately. You‚Äôre a man now, it‚Äôs time to start acting like one...\" she starts ");
    		System.out.println("into another one of her lectures and you are compelled to leave, but stay out of respect. \"Has it ");
-   		System.out.println("occured to you that I might know what Iím talking about? That you're grandmother has a reason for her stories?\"");
+   		System.out.println("occured to you that I might know what I‚Äôm talking about? That you're grandmother has a reason for her stories?\"");
    		System.out.println("she lets out a tired sigh, and grabs a package off of the table. \"Take this to the blacksmith, ");
    		System.out.println("Horace. I need him to do something for me.\" Grateful for any excuse to leave, you take the ");
    		System.out.println("package and leave. As you wander through your humble village your attention is drawn to a rare ");
    		System.out.println("sight; the king's soldiers have arrived.20 years ago the princess of the kingdom disappeared. Now, as the ");
    		System.out.println("king approaches his deathbed, panic has started to seize the kingdom as no new heirs have ");
    		System.out.println("arrison. While the search for the princess had never ended, it reached the state of frantic. ");
-   		System.out.println("People are attacked, their houses searched and accusations thrown. Youíve made it your goal ");
+   		System.out.println("People are attacked, their houses searched and accusations thrown. You‚Äôve made it your goal ");
    		System.out.println("to avoid the guards and topic at all cost. As you near the blacksmith, you overhear the hushed ");
    		System.out.println("conversations of the people in the city square. \"The kidnapper... here\"\n\"In our own... all along\" ");
-   		System.out.println("\"Theyíve already been...\" \n\"... take revenge on all of...\"\nDoing your best to ignore them, ");
+   		System.out.println("\"They‚Äôve already been...\" \n\"... take revenge on all of...\"\nDoing your best to ignore them, ");
    		System.out.println("you enter the blacksmiths. Immediately wrapped in a bear hug, Horace lets out his usual ");
-   		System.out.println("greeting. \"...so big youíve gotten! Now all you need is a hammer and anvil and youíll be a real ");
-   		System.out.println("man in no time!\" you greet him with a smile. Since youíve been alone in your grandmother's ");
+   		System.out.println("greeting. \"...so big you‚Äôve gotten! Now all you need is a hammer and anvil and you‚Äôll be a real ");
+   		System.out.println("man in no time!\" you greet him with a smile. Since you‚Äôve been alone in your grandmother's ");
    		System.out.println("care since youth, this man has been your closest imitation of a father figure. \"Grandmother ");
    		System.out.println("asked me to deliver this to you.\" You hand him the bundle. With a grunt he opens it, inside is ");
    		System.out.println("another box and an envelope. He tears open the letter and reads quitely. Letting out a grunt, he ");
    		System.out.println("walks to the back of the crowded store he returns with a short sword, a shield, and a crossbow. ");
    		System.out.println("A thick sorrow has wormed its way into his expression as he places them into your confused ");
    		System.out.println("arms. The pile is easily worth more than you could make in several years. You open your mouth ");
-   		System.out.println("but he raises a finger to stop you. \"I donít know either. Youíre grandmother asked for no ");
+   		System.out.println("but he raises a finger to stop you. \"I don‚Äôt know either. You‚Äôre grandmother asked for no ");
    		System.out.println("questions.\" \"But. . .\" You start. \"This is the most important part\" he continues, ignoring your confused ");
    		System.out.println("questions. He places the package on top of the pile. \"Now leave.\" his voice drips with emotion, a ");
-   		System.out.println("completely different than the one youíve known. \"I still donít. . .\" \n\""+name+", I'm sorry, but you need to leave.\"\nHis voice cracks. ");
-   		System.out.println("Confused, you run out of the forge. Immediately, youíre hit with the strong smell of smoke.");
+   		System.out.println("completely different than the one you‚Äôve known. \"I still don‚Äôt. . .\" \n\""+name+", I'm sorry, but you need to leave.\"\nHis voice cracks. ");
+   		System.out.println("Confused, you run out of the forge. Immediately, you‚Äôre hit with the strong smell of smoke.");
    		System.out.println("Looking up, you see the city engulfed in quickly spreading flames.");
    		System.out.println("Already forgetting his recent threats, you throw open the door and yell a ");
    		System.out.println("warning to Horace. Without waiting for a response, you run back towards your grandmother's ");
@@ -63,9 +63,9 @@ public class Semester
    		System.out.println("You ride hard, eventually reaching a nearby hunting cabin that Horace often lets you use.");
    		System.out.println("Surrounded by trees and confusion, you decide to start searching for answers. You tear open the letter");
    		System.out.println("and see your grandmother's neat handwriting.");
-   		System.out.println("\"Iím sorry to do this to you, it hasnít been what I planned at all. You must go and ");
-   		System.out.println("retrieve an important item for me. Keep the book Iíve given you safe, follow the compass Iíve left ");
-   		System.out.println("you. It knows the way, even if you donít. Don't worry about me, I'll leave the city as soon as you leave for");
+   		System.out.println("\"I‚Äôm sorry to do this to you, it hasn‚Äôt been what I planned at all. You must go and ");
+   		System.out.println("retrieve an important item for me. Keep the book I‚Äôve given you safe, follow the compass I‚Äôve left ");
+   		System.out.println("you. It knows the way, even if you don‚Äôt. Don't worry about me, I'll leave the city as soon as you leave for");
    		System.out.println("the forge. I'll meet with you as soon as I hear you've retreived what I've asked of you.\""); 
    		
    		//------------------------------------------------------------------------------------------------------------------------------------
@@ -75,6 +75,21 @@ public class Semester
    		System.out.println("\n\nWould you like to go back to the city and search for your grandmother? (Hit 1)\n or, follow the compass(Hit 2)");
    		
       
-    
+    //New story parts
+        ‚ÄúChoosing to follow the compass, you mount the horse. As you resume your journey, you head\n west, straying farther from your hometown and the threat of the guards. You follow the \n unflinching westward point of the compass until nightfall. You feel you're scepticism grow in \n the magic of the bronze compass, and begin to question if you‚Äôre making the right decision. As \n darkness falls, do you:
+Set up camp for the night? 
+Continue to travel in the night?‚Äù
+
+Opt1: ‚ÄúAfter much deliberation, you decide it‚Äôs best to set up camp for the night and try to get some rest. After tying the horse to a nearby tree, you make a small meal for yourself and feed your horse a humble serving of oats.‚Äù As darkness falls, will you make a fire for warmth and protection?:
+Make the fire.
+Don‚Äôt make the fire.
+
+2opt1: ‚ÄúAfter deciding not to risk the cold or the threat of wildlife,  you gather your flint, steel, and nearby wood to make a fire. Exhausted from the events of the day, you quickly fall asleep.\n\nYou wake to the feeling something is off. Not yet rising, you strain your ears against the quiet crackle of the dying fire next to you. You pick up the sound of hushed voices and clumsy feet over leaves. Still feigning sleep, do you:
+Attempt to grab your crossbow.
+Attempt to grab your sword.
+
+3opt1 ‚ÄúAfter rolling quickly rolling over, you grab your crossbow and point it towards the voices. You see two figures stop at the edge of your camp. Identifying them as guards, they stop their advance and 
+
+
     }
 }
